8477ba432ff21b9e96231ad75aeef4046ba3567f
Guilherme Gualberto <guilherme.moreira1997@hotmail.com>
2022-05-18 14:45:11 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,8 +24,13 @@ public class ChessMatch {
 		
 	}
 	
-	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.BLACK), new Position(1,2));
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
+	
+	private void initialSetup() {
+		placeNewPiece('b', 6, new Rook(board, Color.BLACK));
+	}
+
 
 }
