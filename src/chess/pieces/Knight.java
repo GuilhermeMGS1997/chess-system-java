9cf5772e66290bc885382df31771446156cf3f2a
Guilherme Gualberto <guilherme.moreira1997@hotmail.com>
2022-05-23 22:21:23 -0300

    Queen and knight fix

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -15,6 +15,11 @@ public class Knight extends ChessPiece {
 	public String toString() {
 		return "N";
 	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
